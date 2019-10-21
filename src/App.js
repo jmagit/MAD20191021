f87bf159775b1012ce9b350226f9023358a66374
Javier <jmartin@grupoloyal.com>
2019-10-21 18:37:54 +0200

    enviroment

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Editar <code>src/App.js</code> y guardar para recargar.
+          Editar <code>src/App.js</code> y guardar para recargar en modo {process.env.REACT_APP_API_URL}.
         </p>
         <a
           className="App-link"
